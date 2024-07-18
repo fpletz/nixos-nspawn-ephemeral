b4f80496889078a5aa523092a3502ac6a937995d
Franz Pletz <fpletz@fnordicwalking.de>
2024-07-18 20:11:20 +0200

    eval config & path option overide like nixos containers

--- checks.nix
+++ checks.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 {
   perSystem =
-    { pkgs, ... }:
+    { pkgs, system, ... }:
     {
       checks = {
         simple-container = pkgs.nixosTest {
@@ -10,12 +10,10 @@
             { ... }:
             {
               imports = [ inputs.self.nixosModules.host ];
-              virtualisation.nixos-nspawn-ephemeral.containers.test = inputs.nixpkgs.lib.nixosSystem {
-                inherit (pkgs) system;
-                modules = [
-                  inputs.self.nixosModules.container
-                  { networking.hostName = "test-container"; }
-                ];
+              virtualisation.nixos-nspawn-ephemeral.containers = {
+                test.config = {
+                  networking.hostName = "test-container";
+                };
               };
             };
 
@@ -25,6 +23,30 @@
             # needs to wait until networking is configured
             host.wait_until_succeeds("ping -c 1 test")
             host.succeed("machinectl shell test /run/current-system/sw/bin/ping -c 1 host")
+          '';
+        };
+
+        custom-path-eval = pkgs.nixosTest {
+          name = "simple-container";
+          nodes.host =
+            { ... }:
+            {
+              imports = [ inputs.self.nixosModules.host ];
+              virtualisation.nixos-nspawn-ephemeral.containers = {
+                test.path =
+                  (inputs.nixpkgs.lib.nixosSystem {
+                    inherit (pkgs) system;
+                    modules = [
+                      inputs.self.nixosModules.container
+                      { networking.hostName = "test-container"; }
+                    ];
+                  }).config.system.build.toplevel;
+              };
+            };
+
+          testScript = ''
+            start_all()
+            host.wait_for_unit("systemd-nspawn@test.service")
           '';
         };
       };
--- host.nix
+++ host.nix
@@ -6,11 +6,64 @@
 }:
 let
   cfg = config.virtualisation.nixos-nspawn-ephemeral;
+
+  containerModule = lib.types.submodule (
+    {
+      config,
+      options,
+      name,
+      ...
+    }:
+    {
+      options = {
+        config = lib.mkOption {
+          description = ''
+            A specification of the desired configuration of this
+            container, as a NixOS module.
+          '';
+          type = lib.mkOptionType {
+            name = "Toplevel NixOS config";
+            merge =
+              loc: defs:
+              (import "${toString pkgs.path}/nixos/lib/eval-config.nix" {
+                modules = [
+                  {
+                    networking.hostName = lib.mkDefault name;
+                    nixpkgs.hostPlatform = lib.mkDefault pkgs.system;
+                  }
+                  ./container.nix
+                ] ++ (map (x: x.value) defs);
+                prefix = [
+                  "containers"
+                  name
+                ];
+                system = null;
+              }).config;
+          };
+        };
+
+        path = lib.mkOption {
+          type = lib.types.path;
+          example = "/nix/var/nix/profiles/per-container/webserver";
+          description = ''
+            As an alternative to specifying
+            {option}`config`, you can specify the path to
+            the evaluated NixOS system configuration, typically a
+            symlink to a system profile.
+          '';
+        };
+      };
+
+      config = {
+        path = lib.mkIf options.config.isDefined config.config.system.build.toplevel;
+      };
+    }
+  );
 in
 {
   options = {
     virtualisation.nixos-nspawn-ephemeral = {
-      containers = lib.mkOption { type = lib.types.attrs; };
+      containers = lib.mkOption { type = lib.types.attrsOf containerModule; };
     };
   };
 
@@ -24,12 +77,12 @@ in
     };
 
     systemd.nspawn = lib.flip lib.mapAttrs cfg.containers (
-      name: container: {
+      name: containerCfg: {
         execConfig = {
           Ephemeral = true;
           Boot = false;
           PrivateUsers = true;
-          Parameters = "${container.config.system.build.toplevel}/init";
+          Parameters = "${containerCfg.path}/init";
         };
         filesConfig = {
           BindReadOnly = "/nix/store";
